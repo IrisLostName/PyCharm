1a822ba0727ac6a106f2f736ce31d7ec04f6c81c
Takam <74960289+IrisLostName@users.noreply.github.com>
2025-12-19 14:50:18 +0800

    12-19-2

--- 12.19-2.py
+++ 12.19-2.py
@@ -1,7 +1,6 @@
-from cProfile import label
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,9 +13,10 @@ scaler.fit(data_ori[feature])
 data_ori_nor=scaler.transform(data_ori[feature])
 
 n=3
-labels=KMeans(
-    n_clusters=n,
-    random_state=0).fit(data_ori_nor).labels_
+GMM=GaussianMixture(n,random_state=0).fit(data_ori_nor)
+labels=GMM.predict(data_ori_nor)
+num_iter=GMM.n_iter_
+print(num_iter)  # 11
 
 output_data=pd.concat(
     (
@@ -24,18 +24,19 @@ output_data=pd.concat(
     ),axis=1
 )
 
-output_data.to_csv('kmeans聚类结果.csv',index=False)
+output_data.to_csv('GMM聚类结果.csv',index=False)
 
-df = pd.read_csv('kmeans聚类结果.csv')
+df = pd.read_csv('GMM聚类结果.csv')
 sns.set(style="whitegrid")
 plt.figure(figsize=(10, 6))
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 sns.scatterplot(data=df, x='aveSpeed', y='stopNum', hue='labels', palette='viridis', s=100, alpha=0.7)
-plt.title('KMeans聚类结果', fontsize=15)
+plt.title('GMM聚类结果', fontsize=15)
 plt.xlabel('平均速度', fontsize=12)
 plt.ylabel('停靠站数量', fontsize=12)
 plt.legend(title='聚类标签')
-plt.savefig('kmeans聚类可视化.png')
+plt.savefig('GMM聚类可视化.png')
+
 
 
